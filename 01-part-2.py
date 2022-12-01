2a71595f593306d547309a0a6c350a7263a416b7
Felix Schulze Sindern <felixs@fh-muenster.de>
2022-12-01 16:32:42 +0100

    Solve day 01

---
+++ 01-part-2.py
@@ -0,0 +1,6 @@
+lines = "".join(open("01-input", "r").readlines())
+# lines = "".join(open("01-example-input", "r").readlines())
+inventories = lines.split("\n\n")
+inventories = [[int(calory_count) for calory_count in inventory.split("\n")] for inventory in inventories]
+calorie_sums = [sum(inventory) for inventory in inventories]
+print(sum(sorted(calorie_sums, reverse=True)[:3]))
